55d6ea5a760b7ab44ea71e373927f3abefcfd812
SabaMarghania1 <marghanias@gmail.com>
2024-12-27 19:45:18 +0400

    Sample Data && Product List

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/ProductList";
+
 const HomePage = () => {
-  return <>Prostore</>;
+  console.log(sampleData);
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default HomePage;
