2f9f60b2b3921ec7844417b0351dfccb6f765c69
SabaMarghania1 <marghanias@gmail.com>
2025-01-03 09:03:33 +0400

    Removed bcrypts-ts-edge && Implement Custom encryption

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,17 +1,17 @@
-import { hashSync } from "bcrypt-ts-edge";
+import { hash } from "@/lib/encrypt";
 
 const sampleData = {
   users: [
     {
       name: "Saba",
       email: "admin@example.com",
-      password: hashSync("123456", 10),
+      password: hash("123456"),
       role: "admin",
     },
     {
       name: "Gvanca",
       email: "user@example.com",
-      password: hashSync("123456", 10),
+      password: hash("123456"),
       role: "user",
     },
   ],
--- db/seed.ts
+++ db/seed.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import sampleData from "./sample-data";
+import { hash } from "@/lib/encrypt";
 
 async function main() {
   const prisma = new PrismaClient();
@@ -12,8 +13,15 @@ async function main() {
   await prisma.product.createMany({
     data: sampleData.products,
   });
+  const users = [];
+  for (let i = 0; i < sampleData.users.length; i++) {
+    users.push({
+      ...sampleData.users[i],
+      password: await hash(await sampleData.users[i].password),
+    });
+  }
   await prisma.user.createMany({
-    data: sampleData.users,
+    data: users,
   });
 
   console.log("Database seeded succesfully!");
